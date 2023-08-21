e71c6aa20e486e70fae8d889b37829cc63eec4a1
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-08-21 17:38:19 +0530

    Create repeatedSubstring.java

---
+++ repeatedSubstring.java
@@ -0,0 +1,8 @@
+public class repeatedSubstring {
+    class Solution {
+        public boolean repeatedSubstringPattern(String s) {
+           String bigStr = s + s;
+           return bigStr.substring(1, bigStr.length()-1).indexOf(s) != -1;
+        }
+    }
+}
