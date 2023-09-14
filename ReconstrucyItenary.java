b3e40d84591d6ca7a872e1581ea41621074b16ea
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-09-14 15:39:01 +0530

    Create ReconstrucyItenary.java

---
+++ ReconstrucyItenary.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReconstrucyItenary {
+    class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, List<String>> graph = new HashMap<>();
+
+        for (List<String> ticket : tickets) {
+            String departure = ticket.get(0);
+            String arrival = ticket.get(1);
+
+            graph.computeIfAbsent(departure, k -> new ArrayList<>()).add(arrival);
+        }
+
+        for (List<String> destinations : graph.values()) {
+            destinations.sort(Collections.reverseOrder());
+        }
+
+        List<String> newItinerary = new ArrayList<>();
+        Deque<String> stack = new ArrayDeque<>();
+        stack.push("JFK");
+
+        while (!stack.isEmpty()) {
+            String currentAirport = stack.peek();
+
+            if (graph.containsKey(currentAirport) && !graph.get(currentAirport).isEmpty()) {
+                stack.push(graph.get(currentAirport).remove(graph.get(currentAirport).size() - 1));
+            } else {
+                newItinerary.add(stack.pop());
+            }
+        }
+
+        Collections.reverse(newItinerary);
+        return newItinerary;        
+    }
+}
+
+}
