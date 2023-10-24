2349b51f92eac607e4ca0563bd2e6ec5039e1c11
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2023-10-24 13:03:43 +0530

    Create largestValRow.java

---
+++ largestValRow.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+public class largestValRow {
+    class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int max_val = Integer.MIN_VALUE;
+            int levelSize = q.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                max_val = Math.max(max_val, node.val);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+
+            res.add(max_val);
+        }
+
+        return res;        
+    }
+}
+}
