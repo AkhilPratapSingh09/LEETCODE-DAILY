8b386c9f7ace4fb40c4cdca626e6b6cc1db3aae2
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-01-13 09:21:23 +0530

    Create insertionshotll.java

---
+++ insertionshotll.java
@@ -0,0 +1,45 @@
+import org.w3c.dom.Node;
+
+public class insertionshotll {
+    class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        Node curr = head_ref.next;
+        Node prev = head_ref;
+        
+        while(curr != null){
+            
+            if(curr.data >= prev.data){
+                prev = curr;
+                curr = curr.next;
+            }else{
+                
+                int data = curr.data;
+                prev.next = curr.next;
+                curr = curr.next;
+                
+                Node newNode = new Node(data);
+                if(data <= head_ref.data){
+                    newNode.next = head_ref;
+                    head_ref = newNode;
+                }else{
+                    
+                    Node start = head_ref;
+                    
+                    while(start != prev && data > start.next.data){
+                        start = start.next;
+                    }
+                    
+                    newNode.next = start.next;
+                    start.next = newNode;
+                }
+            }
+            
+        }
+        
+        return head_ref;
+    }
+}
+    
+}
