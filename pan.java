981b80ae3d87990ed54740873f03ecbd56fd5ab8
AkhilPratapSingh09 <thakurapsinghsuryavanshi@gmail.com>
2024-03-22 11:05:25 +0530

    Create pan.java

---
+++ pan.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class pan {
+    class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> stack = new Stack();
+        ListNode curr = head;
+        while(curr != null) {
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null && curr.val == stack.pop()) {
+            curr = curr.next;
+        }
+        return curr == null;
+    }
+}
+}
